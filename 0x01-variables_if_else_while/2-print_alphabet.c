cd88662952f78172944e2d84931190540e63f6cb
mwasdex <crowngeemax@gmail.com>
2023-02-17 22:28:40 +0300

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print lowwercase alphabet
+ *
+ * Description: print lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <27)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
