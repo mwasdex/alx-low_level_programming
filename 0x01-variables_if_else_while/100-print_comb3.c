40aa6dad70cc805ca867cbb0923c28bec6278cfb
mwasdex <crowngeemax@gmail.com>
2023-02-20 21:55:57 +0300

    Combination3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+nclude <stdio.h>
+
+/**
+   * main - prints all possible different combinations of two digits
+    *
+     * Description: prints all possible different combinations of two digits
+      *
+       * Return: Always 0 (Success)
+        */
+
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i < 56 || j < 57)
+				{
+				putchar(44);
+				putchar(32);
+				}
+				j++;
+			}
+
+			i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -18,3 +18,5 @@ Write a program that prints the lowercase alphabet in reverse, followed by a new
 Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.
 # 9-print_comb.c
 Write a program that prints all possible combinations of single-digit numbers.
+# 100-print_comb3.c
+Write a program that prints all possible different combinations of two digits.
