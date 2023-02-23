024a98a92c422c169c025ec7c77e3ceed3f898ce
mwasdex <crowngeemax@gmail.com>
2023-02-23 20:58:07 +0300

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all numbers 0 through 9
+ * but no 2&4 and then \n
+ * Return always 0
+ */
+
+void print_most_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i == '2' || i == '4')
+continue;
+_putchar(i);
+}
+_putchar('\n');
+}
