1d4ff6e2ec7b93cb990d3b2668968f06c6fbcabb
mwasdex <crowngeemax@gmail.com>
2023-03-10 10:26:21 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - Print number of arguments
+ * argc: Length of arguments
+ * argv: Arguments
+ * Return 0: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc-1);
+	return (0);
+}
