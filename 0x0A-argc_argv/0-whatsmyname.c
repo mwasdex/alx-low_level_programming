6808aa6165c2923a50abe9c5e145998f7eea26bd
mwasdex <crowngeemax@gmail.com>
2023-06-12 18:02:11 +0300

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,8 @@
  * Return: always 0.
  */
 
-void main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
     printf("%s\n", *argv);
+    return (0);
 }
