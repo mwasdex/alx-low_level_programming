cd66e5ab6507fdbf763243154b0aff045fec8e12
mwasdex <crowngeemax@gmail.com>
2023-07-05 19:30:16 +0000

    _puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,7 +12,9 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 		_putchar('\n');
 
+	else
+	{
 	_putchar(*s);
 	_puts_recursion(s + 1);
-	return (0);
+	}
 }
