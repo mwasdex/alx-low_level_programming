b28e41a69b8bea54d40d0a64e6aa46e97a4e8118
mwasdex <crowngeemax@gmail.com>
2023-07-05 18:30:18 +0000

    _puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string.
+ * @s: pointer to a string.
+ *
+ * Return: (0).
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+		return;
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
