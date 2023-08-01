4d9fcf4ac155b658f32573ebddc1bdfaa435834a
mwasdex <crowngeemax@gmail.com>
2023-08-01 15:42:14 +0300

    insert node int

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,6 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
+
 	for (i = 0; temp && i < idx; i++)
 	{
 		if (i == idx - 1)
@@ -39,5 +40,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 			temp = temp->next;
 	}
+
 	return (NULL);
 }
